8cc53dd016b10d784289f0d78158248eb6f2bbf0
Albina2019 <albinas85@mail.ru>
2024-06-27 04:05:10 +0300

    first commit

---
+++ src/main/java/web/service/CarServicelmp.java
@@ -0,0 +1,33 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class CarServicelmp implements CarService {
+
+    @Override
+    public List<Car> getCars(Integer count) {
+        List<Car> data = new ArrayList<>();
+        data.add(new Car("BMW", 2020, "white"));
+        data.add(new Car("Audi", 2021, "red"));
+        data.add(new Car("KIA", 2022, "black"));
+        data.add(new Car("Honda", 2023, "green"));
+        data.add(new Car("Mercedes", 2024, "yellow"));
+
+        List<Car> cars = new ArrayList<>();
+        if (count>5||Objects.isNull(count)){
+            cars.addAll(data);
+        }
+        else {
+            for (int i = 0; i < count; i++) {
+                cars.add(data.get(i));
+            }
+        }
+        return cars;
+    }
+}
